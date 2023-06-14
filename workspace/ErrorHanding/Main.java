8292e8fa2a06144d3cfac6805319310e4dc4d862
lin <2286763448@qq.com>
2023-06-14 20:38:51 +0800

    Aaron第一次修改，修正异常报错提示

--- workspace/ErrorHanding/Main.java
+++ workspace/ErrorHanding/Main.java
@@ -1,9 +1,17 @@
-package dividedbyzero;
+package dividebyzero;
 
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println(3 / 0);
-    }
+    /**
+     * @param args the command line arguments
+     */
 
+    public static void main(String[] args) {
+        try {
+            System.out.println(3 / 0);
+        } catch (Exception e) {
+            System.out.println("erreur car 3/0,C’est illégal");
+
+        }
+    }
 }

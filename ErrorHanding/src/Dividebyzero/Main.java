ce9e5fbe5d79889c3cce491730e2116348e299e6
Beiningying <yingbeining@foxmail.com>
2023-06-24 00:22:07 +0800

    Leo correction

--- ErrorHanding/src/Dividebyzero/Main.java
+++ ErrorHanding/src/Dividebyzero/Main.java
@@ -1,17 +1,11 @@
-package Dividebywero;
+package dividebyzero;
 
 public class Main {
-
-    /**
-     * @param args the command line arguments
-     */
-
     public static void main(String[] args) {
         try {
-            System.out.println(3 / 0);
-        } catch (Exception e) {
-            System.out.println("erreur car 3/0,C’est illégal");
-
+            System.out.println(3/0);
+        } catch (ArithmeticException e) {
+            System.out.println("Caught ArithmeticException: " + e.getMessage());
         }
     }
 }
